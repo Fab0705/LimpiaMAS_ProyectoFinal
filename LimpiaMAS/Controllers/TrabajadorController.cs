ed8e12a65cc010ac13e534e88eec5a7d65fbef94
Issael Rocha <issaelrocha@gmail.com>
2023-05-15 21:44:48 -0500

    form de trabajadores

---
+++ LimpiaMAS/Controllers/TrabajadorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LimpiaMAS.Controllers
+{
+    public class TrabajadorController : Controller
+    {
+        public IActionResult FormTrabajador()
+        {
+            return View();
+        }
+    }
+}
