341926276b221cbd42c222d454cb553807437216
luis0612005 <luisfernandorodriguezmatto@gmail.com>
2023-05-22 17:34:38 -0500

    Mantenimiento Cliente

---
+++ LimpiaMAS/Controllers/ClienteController.cs
@@ -0,0 +1,40 @@
+﻿using LimpiaMAS.Service;
+using LimpiaMAS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LimpiaMAS.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly iCliente _Cliente;
+        public ClienteController(iCliente cliente)
+        { 
+            _Cliente = cliente;
+        }
+        public IActionResult Index()
+        {
+            return View(_Cliente.GetAllClientes());
+        }
+        [Route("Cliente/Eliminar/{Id}")]
+        public IActionResult eliminar(string id) {
+            _Cliente.Remove(id);
+             return RedirectToAction("IndexACliente");
+        }
+        public IActionResult nuevo() 
+        {
+            return View();
+        }
+        public IActionResult gravar(TbCliente cliente) {
+            _Cliente.Add(cliente);
+            return RedirectToAction("IndexAClliente");
+        }
+        [Route("Cliente/Editar/{Id}")]
+        public IActionResult Editar(string id) {
+            return View(_Cliente.edit(id));
+        }
+        public IActionResult editarCliente(TbCliente cliente) {
+            _Cliente.EditDatails(cliente);
+            return RedirectToAction("IndexACliente");
+        }
+    }
+}
---
+++ LimpiaMAS/Service/ClienteRepository.cs
@@ -0,0 +1,43 @@
+﻿using LimpiaMAS.Models;
+
+namespace LimpiaMAS.Service
+{
+    public class ClienteRepository : iCliente
+    {
+        Limpia_MasC conexion = new Limpia_MasC();
+        public void Add(TbCliente cliente)
+        {
+            try { 
+                conexion.TbClientes.Add(cliente);
+                conexion.SaveChanges();
+            }catch (Exception ex) {
+                Console.WriteLine("Ocurrio un error al grabar al archivo",ex.Message);
+            }
+        }
+
+        public TbCliente edit(string id)
+        {
+            var obj = (from tCli in conexion.TbClientes where tCli.IdCli == id select tCli).Single();
+            return obj;
+        }
+
+        public void EditDatails(TbCliente cliente)
+        {
+            var objModificar = (from tCli in conexion.TbClientes where tCli.IdCli == cliente.IdCli select tCli).Single();
+            objModificar.NomCli = cliente.NomCli;
+            conexion.SaveChanges();
+        }
+
+        public IEnumerable<TbCliente> GetAllClientes()
+        {
+            return conexion.TbClientes;
+        }
+
+        public void Remove(string id)
+        {
+            var obj = (from tbCli in conexion.TbClientes where tbCli.IdCli == id select tbCli).Single();
+            conexion.Remove(obj);
+            conexion.SaveChanges() ;
+        }
+    }
+}
